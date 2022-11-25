9ee88eb9f52191febfd54864988bfd2580ec95d5
Abhishek Barve <46127493+abhishekbarve0612@users.noreply.github.com>
2022-11-25 17:49:59 +0000

    :tada: Add Bootstrap and FontAwesome in project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import Head from "next/head";
+import Script from "next/script";
 import '../global.css'
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossOrigin="anonymous"
+      />
+      <script src="https://kit.fontawesome.com/06e7747196.js" crossorigin="anonymous" />
+      <Component {...pageProps} />
+    </>
+  );
 }
+export default MyApp;
