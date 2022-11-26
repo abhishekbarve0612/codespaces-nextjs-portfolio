5ac3713e60d86e45df7194cc99eca65b246faf09
Abhishek Barve <46127493+abhishekbarve0612@users.noreply.github.com>
2022-11-26 07:00:29 +0000

    :style: Add Dark Mode and Color Scheme

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import styles from './Button.module.scss'
 
 const NavBar = (props: {}) => {
+  const toggleDarkMode = () => {
+    if (document.body.classList.contains('dark-mode')) {
+      document.body.classList.remove('dark-mode');
+    } else {
+      document.body.classList.add('dark-mode');
+    }
+  }
+
   return (
     <>
    <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid mx-3">
+  <div className="container-fluid ml-3">
     <a className="navbar-brand" href="#">ABHISHEK</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -15,7 +23,10 @@ const NavBar = (props: {}) => {
         <a className="nav-link active" aria-current="page" href="#">__about_me</a>
         <a className="nav-link" href="#">__my_projects</a>
         <a className="nav-link" href="#">__connect_with_me</a>
-        <a className="nav-link disabled">__dark_mode</a>
+        <a 
+          className="nav-link btn dark-mode-btn"
+          onClick={toggleDarkMode}
+        >__dark_mode</a>
       </div>
     </div>
   </div>
